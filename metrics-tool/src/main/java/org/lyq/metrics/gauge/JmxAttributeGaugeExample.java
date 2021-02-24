e3c6dfb3dbaab455b8792fecaaad76d05c1ad89a
林永强 <1280362591@qq.com>
2021-02-24 10:33:52 +0800

    提交

--- metrics-tool/src/main/java/org/lyq/metrics/gauge/JmxAttributeGaugeExample.java
+++ metrics-tool/src/main/java/org/lyq/metrics/gauge/JmxAttributeGaugeExample.java
@@ -10,7 +10,10 @@ import java.util.concurrent.TimeUnit;
 
 public class JmxAttributeGaugeExample {
 
+    // 创建一个注册表
     private static final MetricRegistry metricRegistry = new MetricRegistry();
+
+    // 创建一个 ConsoleReporter
     private static final ConsoleReporter consoleReporter = ConsoleReporter
             .forRegistry(metricRegistry)
             .convertRatesTo(TimeUnit.SECONDS)
@@ -19,12 +22,27 @@ public class JmxAttributeGaugeExample {
     public static void main(String[] args) throws MalformedObjectNameException, InterruptedException {
         consoleReporter.start(10,TimeUnit.SECONDS);
 
+        // 向注册表中注册一个 JmxAttributeGauge（堆内存）
         metricRegistry.register(MetricRegistry.name(JmxAttributeGaugeExample.class,"HeapMemoryUsage"),new JmxAttributeGauge(
            new ObjectName("java.lang:type=Memory"),"HeapMemoryUsage"
         ));
 
+        // 向注册表中注册一个 JmxAttributeGauge(非堆内存)
         metricRegistry.register(MetricRegistry.name(JmxAttributeGaugeExample.class,"NonHeapMemoryUsage"),new JmxAttributeGauge(
                 new ObjectName("java.lang:type=Memory"),"NonHeapMemoryUsage"
+        ));
+
+        metricRegistry.register(MetricRegistry.name(JmxAttributeGaugeExample.class,"LoadedClassCount"),new JmxAttributeGauge(
+                new ObjectName("java.lang:type=ClassLoading"),"LoadedClassCount"
+        ));
+
+        //
+        metricRegistry.register(MetricRegistry.name(JmxAttributeGaugeExample.class,"TotalLoadedClassCount"),new JmxAttributeGauge(
+                new ObjectName("java.lang:type=ClassLoading"),"TotalLoadedClassCount"
+        ));
+
+        metricRegistry.register(MetricRegistry.name(JmxAttributeGaugeExample.class,"UnloadedClassCount"),new JmxAttributeGauge(
+                new ObjectName("java.lang:type=ClassLoading"),"UnloadedClassCount"
         ));
 
         Thread.currentThread().join();;
--- metrics-tool/src/main/java/org/lyq/metrics/gauge/RatioGaugeExample.java
+++ metrics-tool/src/main/java/org/lyq/metrics/gauge/RatioGaugeExample.java
@@ -8,9 +8,15 @@ import com.codahale.metrics.RatioGauge;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 监控接口的成功率。
+ */
 public class RatioGaugeExample {
 
+    // 定义一个注册表。
     private static final MetricRegistry metricRegistry = new MetricRegistry();
+
+    // 定义一个控制台 Reporter
     private static final ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry)
             .convertRatesTo(TimeUnit.SECONDS)
             .convertDurationsTo(TimeUnit.SECONDS).build();
@@ -19,8 +25,10 @@ public class RatioGaugeExample {
 
 
     public static void main(String[] args) {
-
+        // 每 10 秒向控制台报告一次。
         reporter.start(10,TimeUnit.SECONDS);
+
+        // 向注册表中注册一个 ratioGauge。
         metricRegistry.gauge("success-rate",() -> new RatioGauge(){
             @Override
             protected Ratio getRatio() {
